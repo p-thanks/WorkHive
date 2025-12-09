c5546b4af2766ebd07cd2f14feff0543ed6576c1
paulthanksgiving <paulthanksgiving2@gmail.com>
2025-12-09 04:29:02 +0100

    read

--- Frontend/src/api/apiClient.js
+++ Frontend/src/api/apiClient.js
@@ -1,7 +1,7 @@
 // src/api/apiClient.js
 import axios from 'axios';
 
-const API_BASE_URL = process.env.REACT_APP_API_URL || 'http://localhost:8080/api';
+const API_BASE_URL = process.env.REACT_APP_API_URL;
 
 const apiClient = axios.create({
   baseURL: API_BASE_URL,
