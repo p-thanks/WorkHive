8bb6ec7bcdc2977e4110c5d471801ff2045baa78
paulthanksgiving <paulthanksgiving2@gmail.com>
2025-12-09 04:39:19 +0100

    api

--- Frontend/src/services/authService.js
+++ Frontend/src/services/authService.js
@@ -1,7 +1,7 @@
 // src/services/authService.js
 import axios from 'axios';
 
-const API_URL = 'http://localhost:8080/api/auth';
+const API_URL = `${process.env.REACT_APP_API_URL}/auth`;
 
 export const login = async (credentials) => {
   const response = await axios.post(`${API_URL}/login`, credentials);
